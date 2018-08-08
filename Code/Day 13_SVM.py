94050c0c4ba44b0497922a31435ec813c55e6834
zhang yongquan <zhyqestudy@163.com>
2018-08-08 20:39:58 +0800

    Update Day 13_SVM.py

--- Code/Day 13_SVM.py
+++ Code/Day 13_SVM.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv('../datasets/Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
