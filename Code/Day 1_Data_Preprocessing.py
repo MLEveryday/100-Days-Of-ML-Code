a83d2caaec1307c8ba33e57880de99f4c0e797a3
zhang yongquan <zhyqestudy@163.com>
2018-08-08 20:39:29 +0800

    Update Day 1_Data_Preprocessing.py

--- Code/Day 1_Data_Preprocessing.py
+++ Code/Day 1_Data_Preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 #Step 2: Importing dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('../datasets/Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 print("Step 2: Importing dataset")
