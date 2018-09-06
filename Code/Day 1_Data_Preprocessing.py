8b03cb1ced7bac5bb20c94fcdc4b42e93edc0457
wengJJ <31797645+wengJJ@users.noreply.github.com>
2018-09-06 22:00:43 +0800

    Update Day 1_Data_Preprocessing.py

--- Code/Day 1_Data_Preprocessing.py
+++ Code/Day 1_Data_Preprocessing.py
@@ -42,7 +42,7 @@ print("Y")
 print(Y)
 
 #Step 5: Splitting the datasets into training sets and Test sets
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
 print("---------------------")
 print("Step 5: Splitting the datasets into training sets and Test sets")
