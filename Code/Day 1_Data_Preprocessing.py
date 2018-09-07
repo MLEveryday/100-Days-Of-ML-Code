38df48c38ff5ce5f2e7bc05f2692b3adae6e3a27
altholin <altholin@outlook.com>
2018-09-07 17:33:37 +0800

    修正issue6讨论结果

--- Code/Day 1_Data_Preprocessing.py
+++ Code/Day 1_Data_Preprocessing.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 
 #Step 2: Importing dataset
+#使用pandas的read_csv方法读取本地文件为数据帧，
+#然后从数据帧中制作自变量和因变量的矩阵和向量
 dataset = pd.read_csv('../datasets/Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
@@ -15,6 +17,8 @@ print("Y")
 print(Y)
 
 #Step 3: Handling the missing data
+#为了不降低ml模型的性能，需要处理丢失数据，用sklearn.pre-
+#processingk库的imputer类完成这项任务
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 imputer = imputer.fit(X[ : , 1:3])
@@ -26,6 +30,7 @@ print("X")
 print(X)
 
 #Step 4: Encoding categorical data
+#分类数据指的是含有标签值而不是数字值的变量，使用labelEncoder类解析
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
@@ -42,6 +47,9 @@ print("Y")
 print(Y)
 
 #Step 5: Splitting the datasets into training sets and Test sets
+#使用train_test_split()方法拆分为测试集合和训练集合（20:80）
+#现在的版本 cross_validation包下的功能都移到model_selection下面了 包括cross_validation 
+#现在0.19应该只是warning,0.20版本可能就会直接报错了 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
 print("---------------------")
@@ -56,10 +64,11 @@ print("Y_test")
 print(Y_test)
 
 #Step 6: Feature Scaling
+#使用StandardScalar类特征标准化或Z值归一化
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+X_test = sc_X.transform(X_test)
 print("---------------------")
 print("Step 6: Feature Scaling")
 print("X_train")
